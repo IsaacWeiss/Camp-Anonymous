6ae95a2eeef8563f261edd35190518c7f89276bb
Unknown <isaacaweiss@gmail.com>
2020-04-23 16:21:23 -0400

    Add card expiration date to form

--- scripts.js
+++ scripts.js
@@ -1,10 +1,28 @@
 "use strict";
 
 function submitContactForm() {
-    let email = document.getElementById("email").value;
+    let email = document.querySelector("#email").value;
     alert("Your comment has been submitted. A response will never be sent to " + email + ".");
 }
 
 function submitPaymentForm() {
     alert("Card declined. Please re-enter your information, or try a different card.\n\nThank you for your social security number, though.");
+}
+
+function populateCardYearInput() {    
+    let yearSelect = document.querySelector("#year");
+    
+    let date = new Date();
+    let year = date.getFullYear();
+
+    // clear any placeholder content
+    while (yearSelect.length > 0)
+        yearSelect.remove(0);
+    
+    // populate with current year and next 10
+    for (let i = 0; i <= 10; i++) {
+        let option = document.createElement("option");
+        option.textContent = year + i;
+        yearSelect.appendChild(option);
+    }
 }
